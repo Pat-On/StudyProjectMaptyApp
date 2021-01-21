bf6a6104ac3f49c90fe0f0edaa4dc2d29ff03fa9
PatOn <mahomed2278@gmail.com>
2021-01-21 17:29:18 +0000

    Leaflet Library

--- script.js
+++ script.js
@@ -11,17 +11,28 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// 227 Geolocation API from browser
+// 227 Geolocation API from browser + // 228 Displaying a Map Using Leaflet Library
 if (navigator.geolocation) // check for older browser if it has this API;
     navigator.geolocation.getCurrentPosition(function (position) {
         const { latitude, longitude } = position.coords;
 
         console.log(latitude, longitude);
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+        //we need in our html element with the id map - empty div for example 
+        //L = is the main function - like namespace 
+        const map = L.map('map').setView([latitude, longitude], 13);
 
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`
+        }).addTo(map);
+
+        L.marker([latitude, longitude]).addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup();
 
     }, // first function when the data is going to be successfully fetched
         function () {
             alert('Could not get your position');
         }, // function executed in case of failure
     );
+
