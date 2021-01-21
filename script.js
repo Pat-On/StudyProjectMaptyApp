e5022568adcdb0537797bcd9a5d341a6e816d747
PatOn <mahomed2278@gmail.com>
2021-01-21 16:09:49 +0000

    Web API - Geolocation
    vid 227

--- script.js
+++ script.js
@@ -10,3 +10,18 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// 227 Geolocation API from browser
+if (navigator.geolocation) // check for older browser if it has this API;
+    navigator.geolocation.getCurrentPosition(function (position) {
+        const { latitude, longitude } = position.coords;
+
+        console.log(latitude, longitude);
+        console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+
+
+    }, // first function when the data is going to be successfully fetched
+        function () {
+            alert('Could not get your position');
+        }, // function executed in case of failure
+    );
