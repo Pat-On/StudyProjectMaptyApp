5db2673088c84e3e4260c1339afb7f0d84feb394
PatOn <mahomed2278@gmail.com>
2021-01-22 09:43:57 +0000

    vid 230
    -variable - taken out of f scope;
    -event sumbit
    -event change in form
    
    Adding mark after two steps:
    1. click on map
    2. submiting the form

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+
+// to get access to values included inside the function 
+let map, mapEvent
 // 227 Geolocation API from browser + 228 Displaying a Map Using Leaflet Library
 if (navigator.geolocation) // check for older browser if it has this API;
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -20,30 +23,21 @@ if (navigator.geolocation) // check for older browser if it has this API;
         console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
         //we need in our html element with the id map - empty div for example 
         //L = is the main function - like namespace 
-        const map = L.map('map').setView([latitude, longitude], 13);
+        map = L.map('map').setView([latitude, longitude], 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors`
         }).addTo(map);
 
 
+        //handling clicks on map
+        map.on('click', function (mapE) {
+            mapEvent = mapE;
+            form.classList.remove('hidden');
+            inputDistance.focus();
 
-        map.on('click', function (mapEvent) {
-            console.log(mapEvent);
-            const { lat, lng } = mapEvent.latlng;
 
-            L.marker([lat, lng])
-                .addTo(map)
-                .bindPopup(L.popup({
-                    autoClose: false,
-                    maxWidth: 250,
-                    minWidth: 100,
-                    closeOnClick: false,
-                    className: 'running-popup',
-                })
-                ).setPopupContent('Workout')
-                .openPopup()
-        })
+        });
 
     }, // first function when the data is going to be successfully fetched
         function () {
@@ -51,3 +45,30 @@ if (navigator.geolocation) // check for older browser if it has this API;
         }, // function executed in case of failure
     );
 
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    //Clear input fields
+    inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = '';
+
+
+    //this one is going to display marker
+    console.log(mapEvent);
+    const { lat, lng } = mapEvent.latlng;
+
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(L.popup({
+            autoClose: false,
+            maxWidth: 250,
+            minWidth: 100,
+            closeOnClick: false,
+            className: 'running-popup',
+        })
+        ).setPopupContent('Workout')
+        .openPopup()
+})
+
+inputType.addEventListener('change', function () {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+})
